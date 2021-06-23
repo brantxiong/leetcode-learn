0b0ce95bca327ce38e75a605259dee066936d226
熊博 <gzxiongbo@corp.netease.com>
2021-06-23 16:07:33 +0800

    first commit

---
+++ 66.加一.py
@@ -0,0 +1,17 @@
+# @before-stub-for-debug-begin
+from python3problem66 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode.cn id=66 lang=python3
+#
+# [66] 加一
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+# @lc code=end
+
