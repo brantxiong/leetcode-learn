6ce8dcc00f3e9dba54ec8e2f1bc50f658710725f
熊博 <gzxiongbo@corp.netease.com>
2021-06-23 16:56:42 +0800

    finish 66

--- week-01/66.加一.py
+++ week-01/66.加一.py
@@ -12,6 +12,28 @@ from typing import *
 # @lc code=start
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        pass
+        i = len(digits) - 1
+        result = []
+        carry = 0
+        added = False
+        while i >= 0:
+            temp = digits[i] + carry
+            if not added:
+                temp += 1
+                added = True
+            # 处理进位
+            if temp >= 10:
+                temp %= 10
+                carry = 1
+            else:
+                carry = 0
+            
+            result.insert(0, temp)
+            
+            i -= 1
+        if carry == 1:
+            result.insert(0, carry)
+
+        return result
 # @lc code=end
 
